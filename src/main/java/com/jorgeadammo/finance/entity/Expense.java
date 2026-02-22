5c2b2e900a5afc764b208f434eff3fce5ab9d590
jorgeadammo <jorgeadammo@gmail.com>
2026-02-22 17:25:29 -0300

    :construction: Developed project entities

---
+++ src/main/java/com/jorgeadammo/finance/entity/Expense.java
@@ -0,0 +1,42 @@
+package com.jorgeadammo.finance.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = true)
+    private String parcel;
+
+    @Column(nullable = true)
+    private LocalDate dueDate;
+
+    @Column(nullable = false)
+    private Boolean paid;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "type_expense_id")
+    private TypeExpense typeExpense;
+
+}
---
+++ src/main/java/com/jorgeadammo/finance/entity/TypeExpense.java
@@ -0,0 +1,24 @@
+package com.jorgeadammo.finance.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TypeExpense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String periodicity;
+}
